c5ec6662ecc55d7b7b3d62b6708bff331851ff15
Eric Ogren <eogren@gmail.com>
2026-08-02 08:06:25 -0700

    Add TokenList class which will be used to do BPE merges. (#9)

---
+++ src/tokenize/token_list_detail.cpp
@@ -0,0 +1,188 @@
+#include "token_list_detail.hpp"
+
+#include "toby/tokenize/vocab.hpp"
+
+#include <cassert>
+#include <concepts>
+#include <cstddef>
+#include <format>
+#include <iterator>
+#include <limits>
+#include <memory>
+#include <ranges>
+#include <span>
+#include <stdexcept>
+#include <utility>
+
+using toby::tokenize::detail::TokenList;
+
+static_assert(std::forward_iterator<TokenList::ConstIterator>);
+static_assert(std::forward_iterator<TokenList::Iterator>);
+static_assert(std::convertible_to<TokenList::Iterator, TokenList::ConstIterator>);
+static_assert(!std::convertible_to<TokenList::ConstIterator, TokenList::Iterator>);
+static_assert(std::ranges::forward_range<TokenList>);
+static_assert(std::ranges::forward_range<const TokenList>);
+
+using toby::tokenize::Vocab;
+
+namespace toby::tokenize::detail {
+TokenList::TokenList(std::shared_ptr<const Vocab> vocab, std::span<const std::byte> bytes_in)
+    : vocab_(std::move(vocab)) {
+    if (bytes_in.size() >= std::numeric_limits<std::ptrdiff_t>::max()) {
+        throw std::runtime_error{"bytes_in has way too many elements, bailing"};
+    }
+    if (vocab_ == nullptr || vocab_->size() == 0) {
+        throw std::invalid_argument{"vocab must not be null or empty"};
+    }
+
+    nodes_.reserve(bytes_in.size());
+    for (const std::byte& b : bytes_in) {
+        auto token = vocab_->token_for_bytes(std::span{&b, 1});
+        if (!token.has_value()) {
+            throw std::invalid_argument{
+                std::format("No token found for {:02x}", std::to_integer<unsigned int>(b))};
+        }
+
+        nodes_.emplace_back(*token, nodes_.size() + 1);
+    }
+
+    check_integrity();
+}
+
+TokenList::Iterator TokenList::begin() {
+    return {this, 0};
+}
+
+TokenList::Iterator TokenList::end() {
+    return {this, nodes_.size()};
+}
+
+TokenList::ConstIterator TokenList::begin() const {
+    return {this, 0};
+}
+
+TokenList::ConstIterator TokenList::end() const {
+    return {this, nodes_.size()};
+}
+
+template <bool IsConst>
+TokenList::BasicIterator<IsConst>::BasicIterator(Owner* owner, std::size_t node)
+    : owner_(owner), node_(node) {}
+
+template <bool IsConst>
+TokenList::BasicIterator<IsConst>::BasicIterator(const BasicIterator<false>& other)
+    requires IsConst
+    : owner_(other.owner_), node_(other.node_) {}
+
+template <bool IsConst>
+TokenList::BasicIterator<IsConst>& TokenList::BasicIterator<IsConst>::operator++() {
+    assert(owner_ != nullptr);
+    assert(node_ < owner_->nodes_.size());
+
+    node_ = owner_->nodes_[node_].next;
+
+    return *this;
+}
+
+template <bool IsConst>
+TokenList::BasicIterator<IsConst> TokenList::BasicIterator<IsConst>::operator++(int) {
+    auto previous = *this;
+    ++(*this);
+    return previous;
+}
+
+template <bool IsConst> const TokenId& TokenList::BasicIterator<IsConst>::operator*() const {
+    assert(node_ < owner_->nodes_.size());
+
+    return owner_->nodes_.at(node_).token;
+}
+
+template <bool IsConst> const TokenId* TokenList::BasicIterator<IsConst>::operator->() const {
+    return std::addressof(operator*());
+}
+
+template <bool IsConst>
+void TokenList::BasicIterator<IsConst>::merge_with_neighbor(TokenId new_token)
+    requires(!IsConst)
+{
+    auto& node = owner_->nodes_.at(node_);
+#ifndef NDEBUG
+    if (!node.active) {
+        throw std::runtime_error{"Iterator pointing at inactive node"};
+    }
+#endif
+
+    if (node.next == owner_->nodes_.size()) {
+        throw std::runtime_error{"Cannot merge at end of sequence"};
+    }
+
+    // me.next = two nodes from now
+    node.token = new_token;
+    node.version += 1;
+    auto& next_node = owner_->nodes_.at(node.next);
+    node.next = next_node.next;
+
+    // me.new_next.prev = me
+    if (node.next != owner_->nodes_.size()) {
+        auto& new_next = owner_->nodes_.at(node.next);
+        new_next.prev = static_cast<std::ptrdiff_t>(node_);
+    }
+
+#ifndef NDEBUG
+    next_node.active = false;
+#endif
+
+    owner_->check_integrity();
+}
+
+template class TokenList::BasicIterator<false>;
+template class TokenList::BasicIterator<true>;
+
+void TokenList::check_integrity() const {
+#ifndef NDEBUG
+    // if not active: next
+    // if active:
+    // next should be active
+    // next->prev should be this if not at end
+    // if at beginning prev should be -1
+    if (nodes_.empty()) {
+        return;
+    }
+
+    const auto& first = nodes_.front();
+    if (!first.active) {
+        throw std::logic_error{"First node should be active"};
+    }
+
+    if (first.prev != -1) {
+        throw std::logic_error{"Previous elem of first node should be -1"};
+    }
+
+    for (size_t cur_index = 0; cur_index < nodes_.size(); cur_index++) {
+        const auto& node = nodes_.at(cur_index);
+        // If node not active, we have nothing to verify
+        if (!node.active) {
+            continue;
+        }
+
+        // If at the end, we don't need to do this next ptr check
+        if (node.next == nodes_.size()) {
+            continue;
+        }
+
+        const auto& next = nodes_.at(node.next);
+        if (!next.active) {
+            throw std::logic_error{std ::format(
+                "Consistency failure: Node {} points at inactive node {}", cur_index, node.next)};
+        }
+
+        const auto& next_prev = nodes_.at(node.next).prev;
+        if (next_prev < 0 || std::cmp_not_equal(next_prev, cur_index)) {
+            throw std::logic_error{
+                std::format("Consistency failure: Node {} has next {}, but next->prev is {}",
+                            cur_index, node.next, next_prev)};
+        }
+    }
+#endif
+}
+} // namespace toby::tokenize::detail
---
+++ src/tokenize/token_list_detail.hpp
@@ -0,0 +1,103 @@
+#pragma once
+
+#include "toby/tokenize/vocab.hpp"
+
+#include <cstddef>
+#include <iterator>
+#include <limits>
+#include <memory>
+#include <span>
+#include <type_traits>
+#include <vector>
+
+namespace toby::tokenize::detail {
+/**
+    TokenList
+*/
+class TokenList {
+public:
+    /**
+      Initialize the token list with the given byte string and vocab. It will first convert every
+      byte in the input string into its corresponding token and throw an exception if an unknown
+      token is found.
+     */
+    TokenList(std::shared_ptr<const Vocab> vocab, std::span<const std::byte> bytes_in);
+
+    template <bool IsConst> class BasicIterator {
+    public:
+        using iterator_concept = std::forward_iterator_tag;
+        using iterator_category = std::forward_iterator_tag;
+        using value_type = TokenId;
+        using difference_type = std::ptrdiff_t;
+        using reference = const TokenId&;
+        using pointer = const TokenId*;
+
+        BasicIterator() = default;
+        ~BasicIterator() = default;
+        BasicIterator(const BasicIterator&) = default;
+        BasicIterator(BasicIterator&&) = default;
+        BasicIterator& operator=(const BasicIterator&) = default;
+        BasicIterator& operator=(BasicIterator&&) = default;
+
+        BasicIterator(const BasicIterator<false>& other)
+            requires IsConst;
+
+        reference operator*() const;
+        pointer operator->() const;
+
+        BasicIterator& operator++();
+        BasicIterator operator++(int);
+
+        friend bool operator==(const BasicIterator&, const BasicIterator&) = default;
+
+        /** Merge the current token with its neighbor, replacing both with new_token. */
+        void merge_with_neighbor(TokenId new_token)
+            requires(!IsConst);
+
+    private:
+        friend class TokenList;
+        template <bool> friend class BasicIterator;
+
+        using Owner = std::conditional_t<IsConst, const TokenList, TokenList>;
+
+        BasicIterator(Owner* owner, std::size_t node);
+
+        Owner* owner_{};
+        std::size_t node_{npos};
+    };
+
+    using Iterator = BasicIterator<false>;
+    using ConstIterator = BasicIterator<true>;
+
+    [[nodiscard]] Iterator begin();
+    [[nodiscard]] Iterator end();
+    [[nodiscard]] ConstIterator begin() const;
+    [[nodiscard]] ConstIterator end() const;
+
+private:
+    /**
+        Check internal integrity of the list and throw
+        an exception if it's invalid
+    */
+    void check_integrity() const;
+
+    struct Node {
+        Node(TokenId token_id, size_t next)
+            : token(token_id), next(next), prev(static_cast<std::ptrdiff_t>(next) - 2) {}
+
+        TokenId token;
+        std::size_t next;
+        // signed so prev can be -1 on the head node
+        std::ptrdiff_t prev;
+        size_t version{};
+#ifndef NDEBUG
+        bool active = true;
+#endif
+    };
+
+    std::vector<Node> nodes_;
+    std::shared_ptr<const Vocab> vocab_;
+
+    static constexpr std::size_t npos = std::numeric_limits<std::size_t>::max();
+};
+} // namespace toby::tokenize::detail
